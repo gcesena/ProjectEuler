2392a04e22cb72f4d140d2fdee543e1db59c7d0c
gcesena <46422132+gcesena@users.noreply.github.com>
2019-01-20 18:47:05 +0100

    chore(0001): Thousand separator
    Using underscore as the thousand separator

--- Java/src/main/java/it/gc/projecteuler/_0001/MultiplesOf3And5.java
+++ Java/src/main/java/it/gc/projecteuler/_0001/MultiplesOf3And5.java
@@ -5,7 +5,7 @@ import java.util.Set;
 public class MultiplesOf3And5 {
 	public static void main(String[] args) {
 		var divisors = Set.of(3, 5);
-		var limit = 1000;
+		var limit = 1_000;
 		var algorithm = new OptimalSolution();
 
 		System.out.println(algorithm.apply(divisors, limit));
