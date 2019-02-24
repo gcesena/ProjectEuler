9e997ea8f6aa002dbf7d3c4048277e5b6c1b6311
gcesena <46422132+gcesena@users.noreply.github.com>
2019-02-24 18:17:34 +0100

    perf(0004): never implement a non interesting optimal solution
    Not always the optimal solution is interesting to implement: in this case, it's not

--- Java/src/main/java/it/gc/projecteuler/_0004/LargestPalindromeProduct.java
+++ Java/src/main/java/it/gc/projecteuler/_0004/LargestPalindromeProduct.java
@@ -1,5 +1,14 @@
 package it.gc.projecteuler._0004;
 
+/*
+ * Why this problem only has the naïve algorithm.
+ *
+ * Because the very last solution, as given in the overview paper, is very specific: not only for the radix it can be
+ * used in (only 10), but also for the number of digits of the factors it can be run with (again, only 3).
+ *
+ * Any implementation would be parameter-less, accompanied by a single trivial test and programmatically non interesting
+ */
+
 public class LargestPalindromeProduct {
 	public static void main(String[] args) {
 		var digits = 3;
